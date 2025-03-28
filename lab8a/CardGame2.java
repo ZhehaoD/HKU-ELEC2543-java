b14f0097b358bef6eec0ca10f80c7225284c6a12
zhehaodn@163.com <zhehaodn@163.com>
2025-03-28 14:53:11 +0800

    source

---
+++ lab8a/CardGame2.java
@@ -0,0 +1,13 @@
+public class CardGame2 {
+
+  private Player[] players;	// players in this game
+
+  // constructor
+  public CardGame2(Player[] players) {
+    this.players = players;
+  }
+
+  public void play() {
+
+  }
+}
---
+++ lab8a/Player.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Player {
+
+  // NO MORE INSTANCE VARIABLE CAN BE DEFINED
+  private String name;  // name of the player
+  private ArrayList<Card> cards; // cards the player has
+  private static MyRandom rnd = new MyRandom();
+
+  // YOU CANNOT MODIFY THIS FUNCTION
+  public Player(String name) {
+    this.name = name;
+    cards = new ArrayList<Card>();
+  }
+
+  // YOU CANNOT MODIFY THIS FUNCTION
+  public String getName() {
+    return name;
+  }
+
+  // cards are ordered from small to large
+  public void addCard(Card card) {
+
+  }
+
+  // YOU CANNOT MODIFY THIS FUNCTION
+  public Card playCard() {
+    if (cards.size() == 0) return null;
+
+    return cards.remove(rnd.nextInt(cards.size())-1);
+  }
+
+  // YOU CANNOT MODIFY THIS FUNCTION
+  public void printHand() {
+    System.out.print(name);
+    System.out.println(" has cards: " + cards);
+  }
+}
