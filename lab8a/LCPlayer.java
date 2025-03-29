97e55411f7030a71eedefe7d7cd1fa62eb8150a4
zhehaodn@163.com <zhehaodn@163.com>
2025-03-29 22:27:19 +0800

    constructor

--- lab8a/LCPlayer.java
+++ lab8a/LCPlayer.java
@@ -1,4 +1,8 @@
 public class LCPlayer extends Player{
+    public LCPlayer(String name) {
+        super(name);
+    }
+
     public Card playCard(){
         if (cards.size() == 0) return null;
         return super.cards.remove(super.cards.size() - 1)
--- lab8a/SCPlayer.java
+++ lab8a/SCPlayer.java
@@ -1,4 +1,8 @@
 public class SCPlayer extends Player{
+    public SCPlayer(String name) {
+        super(name);
+    }
+
     public Card playCard(){
         if (cards.size() == 0) return null;
         return super.cards.remove(super.cards.size(0));
