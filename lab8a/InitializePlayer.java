ddec430b94d54555c18171daac25f93ce119bab9
zhehaodn@163.com <zhehaodn@163.com>
2025-03-30 12:37:41 +0800

    init

--- lab8a/InitializePlayer.java
+++ lab8a/InitializePlayer.java
@@ -4,13 +4,14 @@ public class InitializePlayer {
   public static Player[] getPlayers() {
     Player players[] = new Player[4];
 
+    // Prepare a deck of cards
+    Deck deck = new Deck();
+
     players[0] = new SCPlayer("John");
-    players[1] = new SCPlayer("Mary");
+    players[1] = new CheatPlayer("Mary", deck);
     players[2] = new LCPlayer("Peter");
     players[3] = new LCPlayer("Susan");
 
-    // Prepare a deck of cards
-    Deck deck = new Deck();
 
     // each player gets 5 cards
     for (int i = 0; i < 5; i++) {
