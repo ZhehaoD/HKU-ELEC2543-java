12b5f28afbb76597289c086d4331f1b6700c5e86
zhehaodn@163.com <zhehaodn@163.com>
2025-04-16 13:40:54 +0800

    final

--- lab9/src/Account.java
+++ lab9/src/Account.java
@@ -9,8 +9,30 @@ public class Account {
   }
 
   public void processCheck(Check check) throws NotEnoughFundException {
+    if (balance< check.getAmount()){
+      throw new NotEnoughFundException();
+    }
+    balance-= check.getAmount();
   }
 
   public void processTransaction(ArrayList<Check> checks) {
-  }
+    for(Check check : checks){
+      boolean checked=false;
+      while(!checked){
+        try {
+          System.out.println("The current balance is " + balance);
+          System.out.println("");
+          System.out.println("Processing "+check.toString());
+          processCheck(check);
+          checked = true;
+        }catch(NotEnoughFundException exception) {
+          System.out.println("check cannot be cleared due to not enough fund.");
+          balance += 500;
+          System.out.println("add 500 to the account...");
+        }
+        }
+      }
+    System.out.println("The current balance is " + balance);
+
+    }
 }
