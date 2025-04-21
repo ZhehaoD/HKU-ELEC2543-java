a197436b6947af3d328e5a37a4a8c54063ad429e
zhehaodn@163.com <zhehaodn@163.com>
2025-04-21 16:16:13 +0800

    final

--- lab10/src/BinarySearch.java
+++ lab10/src/BinarySearch.java
@@ -4,14 +4,25 @@ public class BinarySearch {
   // return null if target not found
   //  YOU MUST USE RECURSION TO IMPLEMENT THIS METHOD
   public static Comparable binSearch(int i, int j, Comparable[] list, Comparable target) {
-    return null;
+    if(i>j){
+      return null;
+    }
+
+    if(target.compareTo(list[(i+j)/2])==0){
+      return 1;
+    }
+    else if(target.compareTo(list[(i+j)/2])>0){
+      return binSearch((i+j)/2 + 1, j, list, target);
+    }else{
+      return binSearch(i, (i+j)/2 - 1, list, target);
+    }
+
   }
 
 
   // find target from list[]
   // return null if target not found
   public static Comparable search(Comparable[] list, Comparable target) {
-
     // search the item using method binSearch
     return binSearch(0, list.length-1, list, target);
   }
--- lab10/src/PascalTriangle.java
+++ lab10/src/PascalTriangle.java
@@ -1,5 +1,16 @@
 public class PascalTriangle {
 
   public static int[] computePT(int k) {
+    if (k == 1) {
+      return new int[]{1};
+    }
+    int[] prevRow = computePT(k - 1);
+    int[] currentRow = new int[k];
+    currentRow[0] = 1;
+    currentRow[k - 1] = 1;
+    for (int i = 1; i < k - 1; i++) {
+      currentRow[i] = prevRow[i - 1] + prevRow[i];
+    }
+    return currentRow;
   }
 }
